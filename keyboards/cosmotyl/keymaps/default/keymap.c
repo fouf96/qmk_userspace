ffaa5215c620cdb2749ad0872c6d51480aec9af4
fouf96 <rafthun@gmail.com>
2024-06-13 23:35:52 +0200

    minor fixes, pray

--- keyboards/cosmotyl/keymaps/default/keymap.c
+++ keyboards/cosmotyl/keymaps/default/keymap.c
@@ -1,4 +1,4 @@
-
+// Copyright 2023 QMK
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
@@ -22,6 +22,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,
         KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
         KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,
-                                   KC_LGUI, KC_BSPC, KC_SPC,
-    )
+                                   KC_LGUI, KC_BSPC, KC_SPC
+
 };
